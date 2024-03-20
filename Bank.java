b6d4d15cb8462989d1383fb398f1b1ddf761500b
Kalmesh2 <164139309+Kalmesh2@users.noreply.github.com>
2024-03-20 11:59:49 +0530

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,30 @@
+//write a java program to illustrate the type of constructors in java using object oreanted program concept
+
+class Bank
+{
+		public static void main(String[] args);
+			int ano;
+			float bal;
+		//Default constructor
+	Bank()
+{
+	ano=124;
+	bal=10000;
+	aname=Manju;
+}
+}
+
+//Parameterized constructor
+Bank(int x ,float y ,String z)
+{
+	ano=x;
+	bal=y;
+	aname=z;
+}
+
+void display
+{
+public static void main("account no" + ano);
+public static void main("balance"+ bal);
+public static void main("account name + ano");
+}
